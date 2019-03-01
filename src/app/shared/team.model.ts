c84562d36334d5bfea703fc14557bce2c51e8cb3
Szabolcs Szilágyi <sz.clagyi.89@gmail.com>
2019-03-01 17:18:20 +0100

    countStrength method added to teamModel utilized in detail.compat Statistics

--- src/app/shared/team.model.ts
+++ src/app/shared/team.model.ts
@@ -4,21 +4,28 @@ export class TeamModel {
     public leauge: string;
     public headCoach: string;
     public imagePath: string;
-    public strength: number;
+    private strength: number;
     public victory: number;
     public loss: number;
     public draw: number;
+    private matches: number;
 
     constructor(logo: string, name: string, leauge: string, headCoach: string, image: string,
-                strength = 50, victory = 0, loss = 0, draw = 0) {
+                victory = 0, loss = 0, draw = 0) {
                     this.logo = logo;
                     this.name = name;
                     this.leauge = leauge;
                     this.headCoach = headCoach;
                     this.imagePath = image;
-                    this.strength = strength;
                     this.victory = victory;
                     this.loss = loss;
                     this.draw = draw;
+                    this.matches = this.victory + this.loss;
+
+    }
+
+    countStrength() {
+        this.strength = (this.victory / this.matches) * 100;
+        return this.strength;
     }
 }
--- src/app/shared/teams.service.ts
+++ src/app/shared/teams.service.ts
@@ -11,35 +11,50 @@ export class TeamService {
             'Chelsea FC',
             'EFL',
             'Maurizio Sarri',
-            'https://upload.wikimedia.org/wikipedia/commons/f/f7/Arsenal_2_Chelsea_1_%2834876312101%29.jpg'
+            'https://upload.wikimedia.org/wikipedia/commons/f/f7/Arsenal_2_Chelsea_1_%2834876312101%29.jpg',
+            5,
+            2,
+            1
             ),
         new TeamModel(
             'https://www.footballtickets2go.com/assets/images/Aberdeen-FC.png',
             'Aberdeen FC',
             'Scottish Leauge',
             'Derek McInnes',
-            'https://upload.wikimedia.org/wikipedia/commons/3/3d/Scottish_League_Cup_2014_-_Fan_display.jpg'
+            'https://upload.wikimedia.org/wikipedia/commons/3/3d/Scottish_League_Cup_2014_-_Fan_display.jpg',
+            3,
+            2,
+            1
         ),
         new TeamModel(
             'https://upload.wikimedia.org/wikipedia/commons/1/10/Escudo_real_madrid_1941b.png',
             'Real Madrid',
             'BBVA',
             'Santiago Solari',
-            'https://upload.wikimedia.org/wikipedia/commons/5/54/Real_Madrid_vs_Bayern_Munich.jpg'
+            'https://upload.wikimedia.org/wikipedia/commons/5/54/Real_Madrid_vs_Bayern_Munich.jpg',
+            1,
+            2,
+            0
             ),
         new TeamModel(
             'https://www.footballtickets2go.com/assets/images/Aberdeen-FC.png',
             'Aber FC',
             'SCOT',
             'Derek McInnes',
-            'https://upload.wikimedia.org/wikipedia/commons/3/3d/Scottish_League_Cup_2014_-_Fan_display.jpg'
+            'https://upload.wikimedia.org/wikipedia/commons/3/3d/Scottish_League_Cup_2014_-_Fan_display.jpg',
+            4,
+            0,
+            1
             ),
         new TeamModel(
             'https://m.blog.hu/ns/nst/image/Henya/Vend%C3%A9gszerz%C5%91/chelsea.jpg',
             'Chel FC',
             'EF',
             'Maurizio Sarri',
-            'https://upload.wikimedia.org/wikipedia/commons/f/f7/Arsenal_2_Chelsea_1_%2834876312101%29.jpg'
+            'https://upload.wikimedia.org/wikipedia/commons/f/f7/Arsenal_2_Chelsea_1_%2834876312101%29.jpg',
+            5,
+            0,
+            0
         )
     ];
 
@@ -48,7 +63,7 @@ export class TeamService {
     }
 
     getTeam(index: number) {
-        return this.teams[index]
+        return this.teams[index];
     }
 
     addTeam(item: TeamModel) {
